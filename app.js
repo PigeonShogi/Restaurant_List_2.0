b49cde06156ca03c49cde6d5e7302a9620886c29
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-06-20 17:44:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
 const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
