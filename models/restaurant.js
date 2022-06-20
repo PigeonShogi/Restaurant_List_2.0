b8933033775c24d95bfd2f9efee7243faacd7988
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-06-20 22:33:59 +0800

    feat: add restaurant seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,10 +2,42 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
   id: {
-    type: number,
+    type: Number,
     required: true
   },
   name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  google_map: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: String,
+    required: true
+  },
+  description: {
     type: String,
     required: true
   },
@@ -14,17 +46,3 @@ const restaurantSchema = new Schema({
   }
 })
 module.exports = mongoose.model('Restaurant', restaurantSchema)
-
-
-/* 參考資料
-      "id": 1,
-      "name": "Sababa 沙巴巴中東美食",
-      "name_en": "Sababa Pita Bar",
-      "category": "中東料理",
-      "image": "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/5635/01.jpg",
-      "location": "台北市羅斯福路三段 283 巷 17 號",
-      "phone": "02 2363 8009",
-      "google_map": "https://goo.gl/maps/BJdmLuVdDbw",
-      "rating": 4.1,
-      "description":
-*/ 
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const restaurant = require('../restaurant')
+const Restaurant = require('../restaurant')
+const restaurantsJSON = require('D:/4_GitHub/restaurant_list/restaurant.json')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
@@ -9,4 +10,19 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('mongodb connected!')
+  restaurantsJSON.results.forEach(element => {
+    Restaurant.create({
+      id: element.id,
+      name: element.name,
+      name_en: element.name_en,
+      category: element.category,
+      image: element.image,
+      location: element.location,
+      phone: element.phone,
+      google_map: element.google_map,
+      rating: element.rating,
+      description: element.description,
+    })
+  })
+  console.log('done')
 })
