3c48e50dd1347c7556b61b2c21a71091f27162c7
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-06-22 00:38:40 +0800

    feat: add detail link, update link (without function), delete button (without function)

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-const restaurantList = require('./restaurant.json')
+
+const Restaurant = require('./models/restaurant')
+const RestaurantLean = Restaurant.find().lean()
+
+// const restaurantList = require('./restaurant.json') // 引進資料庫後就不需仰賴 JSON 檔，完工後可刪除此行。
+
 const app = express()
 const port = 3000
 
@@ -20,32 +26,38 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const restaurants = restaurantList.results
-  res.render('index', { restaurants: restaurants })
+  RestaurantLean
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
   const restaurantId = req.params.id
-  const restaurant = restaurantList.results.find(element => {
-    return element.id.toString() === restaurantId
-  })
-  res.render('show', { restaurant: restaurant })
+  RestaurantLean
+    .then(restaurants => {
+      const restaurant = restaurants.find(element => {
+        return element.id.toString() === restaurantId
+      })
+      res.render('show', { restaurant })
+    })
 })
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
-  const searchedRestaurants = restaurantList.results.filter(element => {
-    return element.name.toLowerCase().includes(keyword) ||
-      element.name_en.toLowerCase().includes(keyword) ||
-      element.category.toLowerCase().includes(keyword)
-  })
-  if (searchedRestaurants.length === 0) {
-    const restaurants = restaurantList.results
-    res.render('no_result', { keyword: keyword })
-  } else {
-    res.render('index', { restaurants: searchedRestaurants, keyword: keyword })
-  }
-
+  RestaurantLean
+    .then(restaurant => {
+      const searchedRestaurants = restaurant.filter(element => {
+        return element.name.toLowerCase().includes(keyword) ||
+          element.name_en.toLowerCase().includes(keyword) ||
+          element.category.toLowerCase().includes(keyword)
+      })
+      if (searchedRestaurants.length === 0) {
+        res.render('no_result', { keyword: keyword })
+      } else {
+        res.render('index', { restaurants: searchedRestaurants, keyword: keyword })
+      }
+    }
+    )
 })
 
 app.listen(port, () => {
